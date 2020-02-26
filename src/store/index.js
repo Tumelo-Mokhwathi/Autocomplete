eb291134d3a3b8f1f12dec448d8ef9ca5e4abbe7
Tumelo <tumelo.mokhwathi@gmail.com>
2020-02-26 19:16:42 +0200

    first commit

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,5 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import Axios from "axios";
 
 Vue.use(Vuex);
 
@@ -16,13 +15,20 @@ export default new Vuex.Store({
   actions: {
     ListOfTypes: context => {
       context.commit("Loading", true);
-      Axios.get(`${process.env.VUE_APP_ROOT_API}/autocomplete`)
-        .then(result => {
-          context.commit("List", result.data);
-        })
-        .catch(() => {
-          //error message
-        });
+      context.commit("List", [
+        { id: 1, Address: "Tembisa" },
+        { id: 2, Address: "Olifantsfontein" },
+        { id: 3, Address: "Midrand" },
+        { id: 4, Address: "Pretoria" },
+        { id: 5, Address: "Kempton Park" },
+        { id: 6, Address: "Johannesburg" },
+        { id: 7, Address: "Soweto" },
+        { id: 8, Address: "Polokwane" },
+        { id: 9, Address: "Menlyn" },
+        { id: 10, Address: "Centurion" },
+        { id: 11, Address: "Sandton" },
+        { id: 12, Address: "Edenvale" }
+      ]);
       context.commit("Loading", false);
     }
   },
